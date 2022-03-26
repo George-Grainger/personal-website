faa074c30793fbda63d6124fc94a75e4e7c3347c
u87612gg <george.grainger-3@student.manchester.ac.uk>
2022-03-26 19:04:03 +0000

    Add breakpoints to navbar for responsivity

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -18,7 +18,11 @@ const Navbar = () => {
       <li>
         <DarkmodeToggle />
       </li>
-      {/* <li>Hamburger menu</li> */}
+      <li>
+        <svg className={styles.hamburger} viewBox="0 0 22 22" xmlns="http://www.w3.org/2000/svg">
+          <path d="M22 22H0V18.3333H22V22ZM22 12.8333H0V9.16667H22V12.8333ZM22 3.66667H0V0H22V3.66667Z" />
+        </svg>
+      </li>
     </nav>
   );
 };
